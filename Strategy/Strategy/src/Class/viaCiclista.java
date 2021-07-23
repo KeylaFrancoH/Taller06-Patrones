1bc51b0b7aea3de84c1fa6967b245673bf742061
lalitard <lalitard@espol.edu.ec>
2021-07-22 21:43:42 -0500

    Realizando codigo del patron Strategy

---
+++ Strategy/Strategy/src/Class/viaCiclista.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import Interface.TransportStrategy;
+
+/**
+ *
+ * @author adria
+ */
+public class viaCiclista implements TransportStrategy {
+
+    @Override
+    public void tiposTransporte() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void doSomething() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
